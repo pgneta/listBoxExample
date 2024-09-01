133403a82efddef14b26db0d016ccd52a49e4899
npalombo <npalombo@chegg.com>
2024-09-01 11:22:16 +0300

    sort preselected, removed comments, added TODO

--- src/components/ExampleMultiSelectListBoxDynamicSelector.tsx
+++ src/components/ExampleMultiSelectListBoxDynamicSelector.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Item, ListBoxProps} from "./types";
 import {ButtonsContainer, Container, List, ListItem, SearchInput, ShowButton} from "./styled";
+import {find} from "styled-components/test-utils";
 
 export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = ({
                                                                                       initialSelectedIds,
@@ -12,6 +13,7 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
     const [searchTerm, setSearchTerm] = useState<string>('');
     const [page, setPage] = useState<number>(0);
     const [preSelectedItems, setPreSelectedItems] = useState<Item[]>([]);
+    const [userSelectedItems, setUserSelectedItems] = useState<Item[]>([]);
     const latestSearchRequestRef = useRef<number>(0);
     const [loadingCount, setLoadingCount] = useState<number>(0);
     const [fetchPreSelectedCompleted, setFetchPreSelectedCompleted] = useState<boolean>(false);
@@ -26,7 +28,7 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
             setLoadingCount(prevCount => prevCount + 1);
             try {
                 const fetchedPreSelected = await getItemsById(initialSelectedIds);
-                setPreSelectedItems(fetchedPreSelected);
+                setPreSelectedItems(sortItems(fetchedPreSelected));
             } catch (error) {
                 console.error("Error fetching initial items:", error);
             } finally {
@@ -36,9 +38,8 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
         };
 
         fetchPreSelectedItems();
-    }, [getItemsById, initialSelectedIds]);
+    }, []);
 
-    // Fetch items based on search term and page
     useEffect(() => {
         if (!fetchPreSelectedCompleted) return; // Ensure this fetch runs only after the initial fetch
         const fetchItems = async () => {
@@ -48,7 +49,6 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
                 const fetchedItems = await searchFunction(searchTerm, page);
 
                 if (requestId !== latestSearchRequestRef.current) {
-                    //setLoadingCount(prevCount => prevCount - 1);
                     return;
                 }
                 setFetchedNewData(fetchedItems.length !== 0);
@@ -59,28 +59,28 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
 
                     // Filter out preSelectedItems
                     const filteredItems = combinedItems.filter(
-                        (i) => !preSelectedItems.some((item) => item.value === i.value)
+                        (i) => !preSelectedItems.find((item) => item.value === i.value)
                     );
 
                     // Deduplicate filteredItems
                     const deduplicatedItems = filteredItems.reduce((acc, item) => {
                         if (!acc.find(existingItem => existingItem.value === item.value)) {
+                            // if it was selected by user - we keep user's selection
+                            if (userSelectedItems.find(selectedItem=> selectedItem.value === item.value)) {
+                                item.selected = true;
+                            }
                             acc.push(item);
                         }
                         return acc;
                     }, [] as Item[]);
 
                     // Sort deduplicatedItems by name
-                    return deduplicatedItems.sort((a, b) => {
-                        const valueA = parseInt(a.value, 10);
-                        const valueB = parseInt(b.value, 10);
-                        return valueA - valueB;
-                    });
-
+                    return sortItems(deduplicatedItems);
                 });
 
             } catch (error) {
                 console.error("Error fetching items:", error);
+                //TODO: handle error with relevant LOGGER or UI
             } finally {
                 setLoadingCount(prevCount => prevCount - 1);
             }
@@ -90,9 +90,15 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
     }, [searchTerm, page, fetchPreSelectedCompleted, preSelectedItems, searchFunction]);
 
 
-
-    // Toggle the selected state of the item
     const toggleItemSelectionState = (itemValue: string) => {
+        setUserSelectedItems((preSelectedItems)=>{
+            const item = preSelectedItems.find(i => i.value === itemValue);
+            if (item) {
+                return preSelectedItems.filter(i => i.value !== itemValue);
+            }
+            return [...preSelectedItems, {name: itemValue, value: itemValue}];
+        });
+
         setFetchedItems((prevItems) =>
             prevItems.map(item =>
                 item.value === itemValue ? {...item, selected: !item.selected} : item
@@ -111,13 +117,6 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
         setPreSelectedItems((prevItems) => {
             const item = prevItems.find(i => i.value === itemValue);
             if (item) {
-                // check if it should be kept on current search fetched items:
-                setFetchedItems((prevItems) =>
-                    prevItems.map(item =>
-                        item.value === itemValue ? {...item, selected: false} : item
-                    )
-                );
-                // Remove item from pre-selected list if it is no longer selected
                 return prevItems.filter(i => i.value !== itemValue);
             }
             return prevItems;
@@ -141,7 +140,7 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
     };
 
     const TotalItems: React.FC = () => {
-        return <div>Total items: {fetchedItems.length + preSelectedItems.length}</div>//, page: {page} , loading: {loadingCount}</div>;
+        return <div>Total items: {fetchedItems.length + preSelectedItems.length}</div>
     };
 
     const Loading: React.FC = () => {
@@ -150,6 +149,15 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
 
     const disableShowMore = () => !fetchedNewData || fetchedItems.length + preSelectedItems.length < pageSize ;
 
+    const sortItems = (items: Item[]) => {
+        return items.sort((a, b) => {
+            const valueA = parseInt(a.value, 10);
+            const valueB = parseInt(b.value, 10);
+            return valueA - valueB;
+        });
+    }
+
+///////////////////////////// LIST-BOX COMPONENT /////////////////////////////////////////
 
     return (
         <Container>
@@ -175,7 +183,6 @@ export const ExampleMultiSelectListBoxDynamicSelector: React.FC<ListBoxProps> = 
                 ))}
             </List>
             {loadingCount > 0 ? <Loading/> : <TotalItems/>}
-            {/*{loadingCount === 0 && <TotalItems/>}*/}
             <ButtonsContainer>
                 <ShowButton onClick={handleShowMore} disabled={disableShowMore()}>Show More</ShowButton>
                 <ShowButton onClick={handleShowLess} disabled={page === 0}>Show Less</ShowButton>
